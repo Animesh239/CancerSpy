42f22a6018b619a22e116c32e2dfc60cd8ac2210
Animesh239 <animeshbarik239@gmail.com>
2023-09-08 21:19:39 +0530

    1st commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ResponsiveAppBar from "./components/appbar";
+
+
+
+const App = () => {
+  return (
+    <>
+      <ResponsiveAppBar />
+    </>
+  );
+};
+
+export default App;
